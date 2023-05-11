f685a837a036b666f35e0d8d517c51e65ffeeece
jeffersonmacedocarvalho <131269358+jeffersonmacedocarvalho@users.noreply.github.com>
2023-05-11 09:14:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const nome = prompt("informe seu nome:")
+console.log(nome + " muito bem.");
+
+const idade = Number(prompt(nome + " infrome sua idade:"))
+console.log(idade + " muito bem " + nome);
+
+console.log(idade >= 18);
+
+const data = Number(prompt("informe sua data de nascimento:"))
+let x = 2050 - data
+
+console.log("sua idade em 2050 será:" + x);
+
